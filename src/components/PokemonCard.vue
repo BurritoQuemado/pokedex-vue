f1870445ea8aceedf431d67527fdddd4fda42284
Diego Flores <dfmonroy07@gmail.com>
2024-09-22 14:42:48 -0600

    update bg opacity

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -10,41 +10,41 @@
     const getClass = (type) => {
       switch (type) {
         case 'fire':
-          return "bg-fire-primary text-fire-secondary/80 border-fire-secondary";
+          return "bg-fire-primary/90 text-fire-secondary/80 border-fire-secondary";
         case 'water':
-          return "bg-water-primary text-water-secondary/80 border-water-secondary";
+          return "bg-water-primary/90 text-water-secondary/80 border-water-secondary";
         case 'grass':
-          return "bg-grass-primary text-grass-secondary/80 border-grass-secondary";
+          return "bg-grass-primary/90 text-grass-secondary/80 border-grass-secondary";
         case 'electric':
-          return "bg-electric-primary text-electric-secondary/80 border-electric-secondary";
+          return "bg-electric-primary/90 text-electric-secondary/80 border-electric-secondary";
         case 'ice':
-          return "bg-ice-primary text-ice-secondary/80 border-ice-secondary";
+          return "bg-ice-primary/90 text-ice-secondary/80 border-ice-secondary";
         case 'fighting':
-          return "bg-fighting-primary text-fighting-secondary/80 border-fighting-secondary";
+          return "bg-fighting-primary/90 text-fighting-secondary/80 border-fighting-secondary";
         case 'poison':
-          return "bg-poison-primary text-poison-secondary/80 border-poison-secondary";
+          return "bg-poison-primary/90 text-poison-secondary/80 border-poison-secondary";
         case 'ground':
-          return "bg-ground-primary text-ground-secondary/80 border-ground-secondary";
+          return "bg-ground-primary/90 text-ground-secondary/80 border-ground-secondary";
         case 'flying':
-          return "bg-flying-primary text-flying-secondary/80 border-flying-secondary";
+          return "bg-flying-primary/90 text-flying-secondary/80 border-flying-secondary";
         case 'psychic':
-          return "bg-psychic-primary text-psychic-secondary/80 border-psychic-secondary";
+          return "bg-psychic-primary/90 text-psychic-secondary/80 border-psychic-secondary";
         case 'bug':
-          return "bg-bug-primary text-bug-secondary/80 border-bug-secondary";
+          return "bg-bug-primary/90 text-bug-secondary/80 border-bug-secondary";
         case 'rock':
-          return "bg-rock-primary text-rock-secondary/80 border-rock-secondary";
+          return "bg-rock-primary/90 text-rock-secondary/80 border-rock-secondary";
         case 'ghost':
-          return "bg-ghost-primary text-ghost-secondary/80 border-ghost-secondary";
+          return "bg-ghost-primary/90 text-ghost-secondary/80 border-ghost-secondary";
         case 'steel':
-          return "bg-steel-primary text-steel-secondary/80 border-steel-secondary";
+          return "bg-steel-primary/90 text-steel-secondary/80 border-steel-secondary";
         case 'dragon':
-          return "bg-dragon-primary text-dragon-secondary/80 border-dragon-secondary";
+          return "bg-dragon-primary/90 text-dragon-secondary/80 border-dragon-secondary";
         case 'dark':
-          return "bg-dark-primary text-dark-secondary/80 border-dark-secondary";
+          return "bg-dark-primary/90 text-dark-secondary/80 border-dark-secondary";
         case 'fairy':
-          return "bg-fairy-primary text-fairy-secondary/80 border-fairy-secondary";
+          return "bg-fairy-primary/90 text-fairy-secondary/80 border-fairy-secondary";
         case 'normal':
-          return "bg-normal-primary text-normal-secondary/80 border-normal-secondary";
+          return "bg-normal-primary/90 text-normal-secondary/80 border-normal-secondary";
       }
     };
 </script>
